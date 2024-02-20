f8e806e64ab3b49da827b0f0484e51bb095031dd
SamuelRiveros <156517817+SamuelRiveros@users.noreply.github.com>
2024-02-20 17:48:48 -0500

    feat: :bug: Finalizacion del proyecto
    pequeño arreglo del menu-btn, y finalizado agregando js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
 import {} from "./module/facturacion.js"
+
+const menu = document.querySelector(".menu");
+const menuBtn = document.querySelector(".menu-btn");
+
+menuBtn.addEventListener('click', () => {
+    menu.classList.toggle("nav-toggle");
+});
